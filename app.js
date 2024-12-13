902a3faf17a1002fbc8fce86f17a2ac27cd612e1
Natasha C <n.coward@hotmail.com>
2024-12-13 13:38:35 +0000

    Added small images to the webpage

--- app.js
+++ app.js
@@ -25,10 +25,20 @@ const images = [
 
 const thumbnailSize = 50;
 
-function createThumbnails(imagesArray) {
-  for (let i = 0; i < images.length; i++) {
-    console.log("Check", i);
-    const imgSrc = images[i].src;
-    const imgAlt = images[i].alt;
-  }
-}
+
+// Used the code below from an answer on a website. Changed the links to my URLs for food images and it does update it, but I'm not sure this is what I need (https://stackoverflow.com/questions/42327498/output-an-array-of-images-in-javascript/42327539#42327539)
+//Played around with the body to reference the thumbnails container but couldn't get it to work
+
+var ArrayOfImages = [
+  "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FSeries%2F2024-07-how-to-make-perfect-cheesecake%2Fhow-to-make-perfect-cheesecake-319",
+  "https://cdn.media.amplience.net/i/japancentre/recipes-1541-uramaki-inside-out-sushi-roll/Uramaki-inside-out-sushi-roll?$poi$&w=1200&h=630&sm=c&fmt=auto",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk1pR4Kxyl16suyskuB4zbA4BaNf2XA9yPkw&s",
+  "https://www.allrecipes.com/thmb/dF2YLgfKMy2Ee_kBskeoMBnxKkk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/AR-56803-very-chocolate-ice-cream-Beauties-2x1-9186fbd502194b379eae3041f014fd37.jpg",
+];
+ArrayOfImages.forEach(function (image) {
+  var img = document.createElement("img");
+  img.src = image;
+  img.height = "45";
+  img.width = "50";
+  document.body.appendChild(img);
+});
